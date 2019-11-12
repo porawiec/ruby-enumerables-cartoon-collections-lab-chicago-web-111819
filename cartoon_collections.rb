c497089556235bc873bfcf63bb616bc3c8f1f5ca
Paul O <paul.orawiec@gmail.com>
2019-11-12 10:04:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ def summon_captain_planet(planeteer_calls)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(assorted_words)
   # Your code here
 end
 
