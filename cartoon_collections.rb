40d16ae5b9caaa31929922ce920a05899cfe66d8
Paul O <paul.orawiec@gmail.com>
2019-11-12 10:16:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
 def roll_call_dwarves(dwarves)
-  # Your code here
+  dwarves.each.with_index(1) do | name, index |
+    puts "#{index} #{name}"
+  end
 end
 
 def summon_captain_planet(planeteer_calls)
