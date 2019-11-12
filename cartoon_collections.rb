4bdcf26c5ddfbe431e09be0ff6cd6560ed8344f9
Paul O <paul.orawiec@gmail.com>
2019-11-12 10:57:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,7 @@ end
 def find_the_cheese(strings_array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if { |x| x.include?(cheese_types)}
-    strings_array.find { |y| y == 
+    strings_array.find { |y| y == cheese_types}
   else
     nil
   end
